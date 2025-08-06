41fcfd5931d08c78a75f8c4c6057fce30ba3a464
aayushsiwa <22052177@kiit.ac.in>
2025-08-06 09:15:28 +0530

    feat: parseDate for following yyyy-mm-dd

---
+++ backend/utils/parseDate.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"errors"
+	"log"
+	"strings"
+	"time"
+)
+
+var dateFormats = []string{
+	"2006-01-02",      // 2023-07-25
+	"02-01-2006",      // 25-07-2023
+	"02/01/2006",      // 25/07/2023
+	"01/02/2006",      // 07/25/2023
+	"1/2/2006",        // 7/25/2023
+	"2 Jan 2006",      // 25 Jul 2023
+	"2 January 2006",  // 25 July 2023
+	"January 2, 2006", // July 25, 2023
+	"Jan 2, 2006",     // Jul 25, 2023
+	"2006/01/02",      // 2023/07/25
+	"2006.01.02",      // 2023.07.25
+	"02.01.2006",      // 25.07.2023
+}
+
+func ParseDate(dateStr string) (string, error) {
+	log.Println("Parsing date:", dateStr)
+	dateStr = strings.TrimSpace(dateStr)
+
+	for _, format := range dateFormats {
+		if t, err := time.Parse(format, dateStr); err == nil {
+			return t.Format("2006-01-02"), nil
+		}
+	}
+
+	return "", errors.New("unrecognized date format: " + dateStr)
+}
