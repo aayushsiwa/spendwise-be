85ed52fc0cafc97e3561dce74aa278b5d7d7b18e
aayushsiwa <22052177@kiit.ac.in>
2025-08-06 22:56:39 +0530

    refactor: update dropData.sql
    also drop categories and summary

--- backend/sql/dropData.sql
+++ backend/sql/dropData.sql
@@ -1,5 +1,9 @@
 -- Remove all rows from the records table
 DELETE FROM records;
+DELETE FROM categories;
+DELETE FROM summary;
 
 -- Reset AUTOINCREMENT counter if used
 DELETE FROM sqlite_sequence WHERE name='records';
+DELETE FROM sqlite_sequence WHERE name='categories';
+DELETE FROM sqlite_sequence WHERE name='summary';
