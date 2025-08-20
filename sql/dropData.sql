0428f30e574f102d35211e3620c8be7dbcf2aac6
aayushsiwa <22052177@kiit.ac.in>
2025-08-20 18:47:34 +0530

    feat: summary_details table added

--- sql/dropData.sql
+++ sql/dropData.sql
@@ -2,8 +2,10 @@
 DELETE FROM records;
 DELETE FROM categories;
 DELETE FROM summary;
+DELETE FROM summary_details;
 
 -- Reset AUTOINCREMENT counter if used
 DELETE FROM sqlite_sequence WHERE name='records';
 DELETE FROM sqlite_sequence WHERE name='categories';
 DELETE FROM sqlite_sequence WHERE name='summary';
+DELETE FROM sqlite_sequence WHERE name='summary_details';
--- sql/init.sql
+++ sql/init.sql
@@ -18,9 +18,17 @@ CREATE TABLE IF NOT EXISTS records (
 
 CREATE TABLE IF NOT EXISTS summary (
   month TEXT PRIMARY KEY,  -- format: YYYY-MM
-  total_income REAL DEFAULT 0,
-  total_expense REAL DEFAULT 0,
-  opening_balance REAL DEFAULT 0,
-  net_balance REAL DEFAULT 0,
-  closing_balance REAL DEFAULT 0
+  total_income REAL NOT NULL DEFAULT 0,
+  total_expense REAL NOT NULL DEFAULT 0,
+  opening_balance REAL NOT NULL DEFAULT 0,
+  net_balance REAL NOT NULL DEFAULT 0,
+  closing_balance REAL NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS summary_details (
+  month TEXT NOT NULL,  -- format: YYYY-MM
+  type TEXT NOT NULL CHECK (type IN ('income', 'expense','transfer')),
+  category_id INTEGER NOT NULL,
+  category_name TEXT NOT NULL,
+  amount REAL NOT NULL
 );
